93b5b77485e5c67ebf72cdc951219a1c3e3f06ea
Efe Evci <efeberkeevci@gmail.com>
2021-08-25 00:19:35 -0700

    Handled the arrangement of the calendar and things done part

---
+++ personal-website/src/TodayFocus.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import "./Styles/TodayFocus.css";
+let activites_dom = [];
+let activities = [];
+class TodayFocus extends Component {
+    /*
+    componentWillMount(){
+        activities = this.props.activities;
+        
+        for (let activity of activities) {
+            activites_dom.push( < h5 className = "task" > {activity.title} < /h5>);
+        }
+    }
+    */
+   
+    render() {
+        return ( 
+            <div className = "todayfocus_container" >
+                <h1 className = "title" > Today < /h1>  
+                < h5 className = "task" > Personal Website Maintenance < /h5>
+                < h5 className = "task" > Personal Website Maintenance < /h5>
+                < h5 className = "task" > Personal Website Maintenance < /h5>
+
+             </div >
+        );
+    }
+}export default TodayFocus;
